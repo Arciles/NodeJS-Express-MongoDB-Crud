a8e789d1b5d3197fb03868c09555051432438df8
Esat IBIS <esat.taha.ibis@gmail.com>
2017-01-18 17:13:51 -0500

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var db;
 MongoClient.connect('mongodb://admin:Password123@ds151078.mlab.com:51078/star-wars-quotes', (err, database) => {
   if (err) return console.log(err)
     db = database;
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
 });
 
 // THIS handles GET Requtest
